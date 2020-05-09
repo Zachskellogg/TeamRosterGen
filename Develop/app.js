80e174829b12db2a74687a1ccc5fd800ccbc0d0b
ZachKellogg <zachskellogg@gmail.com>
2020-05-08 20:01:49 -0600

    homework hack athon

--- Develop/app.js
+++ Develop/app.js
@@ -10,10 +10,80 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function init {
+    function createManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the managers name?"
+            },
+            {
+                type: "input",
+                name:"managerId",
+                message: "what is the managers ID number"
+            },
+            {
+                type: "input",
+                name:"managerEmail",
+                message: "what is the managers email number"
+            },
+            {
+                type: "input",
+                name:"managerNumber",
+                message: "what is the managers office number"
+            }
+        ]).then((res) => {
+            // console.log(responses);
+            const manger = new Manager(Res.managerName, res.managerId, res.managerEmail, res.managerNumber);
+            // console.log(manager);
+            createTeam();
+        })
+    }
 
+    function createTeam() {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "What is the role of the next team member?",
+                choices: ["Intern", "Engineer", "None"]
+            }
+        ]).then((res) => {
+            switch (memberChoice) {
+                case "Engineer":
+                    createEngineer();
+                break;
+                case "Intern":
+                    createIntern();
+                break;
+                default:
+                    buildTeam();
+                    break;
+            } 
+            const team =[];
+            const teamMember = res.memberChoice
+            team.push(teamMember);
+        })
+    }
+
+    function createEngineer() {
+
+    }
+
+    functions createIntern() {
+
+    }
+
+    function buildTeam() {
+        
+    }
+    createManager();
+    
+}
+init();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
